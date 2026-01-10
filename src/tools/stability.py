89c63cbc5a21adf9a47ef759e084f7753d2252ee
Hugo <zcaphwh@gmail.com>
2026-01-10 15:46:18 +0000

    Implement subsampling in rank stability function for improved performance

--- src/tools/stability.py
+++ src/tools/stability.py
@@ -107,9 +107,13 @@ def rank_stability(tensor_data, rank, mask=None, n_repeats=10, verbose=0):
 
     # Loop over n_repeats
     for i in range(n_repeats):
-
+        
+        ntrials = tensor_data.shape[0]
+        indices = torch.randperm(ntrials)[:int(ntrials*0.8)]
+        subsampled_data = tensor_data[indices]
+        
         cp_tensor = non_negative_parafac(
-            tensor_data,
+            subsampled_data,
             rank=rank,
             init="random",
             mask=mask,
